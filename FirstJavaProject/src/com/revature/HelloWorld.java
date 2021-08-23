d084b7c4b61f5b0e5c2db1973dc46bcd88fe9218
long-wayne <pho.le3043@gmail.com>
2021-08-23 12:31:49 -0700

    add HelloWorld class

---
+++ FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//syso + ctrl + space will automatically populate the method for you
+		System.out.println("Hello world... from the IDE!");
+
+	}
+
+}
